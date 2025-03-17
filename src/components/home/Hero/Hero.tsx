f493266054fece039ee17b7213edf06f88fdf500
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-16 20:57:00 -0500

    Change styles calling mode in the Hero section

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import styles from './Hero.module.scss';
 
 export const Hero = () => {
   return (
-    <section className={styles.Hero}>
+    <section className={styles["Hero"]}>
       <h1>Camila & Juan Pablo</h1>
     </section>
   );
