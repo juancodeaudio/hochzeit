16855fc0087a945672d4f6332809a088e0339331
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-19 22:01:40 -0500

    feat(Navbar): :sparkles: Add image display and hover effects to navbar links

--- src/components/shared/Navbar/NavbarContent/index.tsx
+++ src/components/shared/Navbar/NavbarContent/index.tsx
@@ -3,18 +3,20 @@ import { motion } from "motion/react";
 import styles from "./NavbarContent.module.scss";
 
 const navLinks = [
-  { href: "/", label: "Home" },
-  { href: "/story", label: "Historia" },
-  { href: "/gallery", label: "Galeria" },
-  { href: "/party", label: "Sobre la fiesta" },
-  { href: "/faq", label: "FAQ" },
+  { href: "/", label: "Home", image: "images/IMG_1704.jpeg" },
+  { href: "/story", label: "Historia", image: "images/IMG_1338.jpeg" },
+  { href: "/gallery", label: "Galeria", image: "images/IMG_2098.jpeg" },
+  { href: "/party", label: "Sobre la fiesta", image: "images/IMG_1704.jpeg" },
+  { href: "/faq", label: "FAQ", image: "images/IMG_2098.jpeg" },
 ];
 
 type NavbarContentProps = {
   toggleNavbar: () => void;
+  setHoveredOption: (option: string) => void;
+  setRandomRotation: (rotation: number) => void;
 };
 
-export const NavbarContent = ({ toggleNavbar }: NavbarContentProps) => {
+export const NavbarContent = ({ toggleNavbar, setHoveredOption, setRandomRotation }: NavbarContentProps) => {  
   const container = {
     hidden: { opacity: 0 },
     show: {
@@ -31,6 +33,12 @@ export const NavbarContent = ({ toggleNavbar }: NavbarContentProps) => {
     show: { x: "0%" }
   }
 
+  const handleLinkHover = (image: string) => {
+    const randomNumber = Math.floor(Math.random() * 17) -8;
+    setRandomRotation(randomNumber);
+    setHoveredOption(image);
+  };
+
   return (
     <motion.ul
       className={styles["navbar__menu"]}
@@ -44,6 +52,8 @@ export const NavbarContent = ({ toggleNavbar }: NavbarContentProps) => {
           key={link.href} 
           variants={item} 
           transition={{ duration: 1, ease: [0.645, 0.045, 0.355, 1] }}
+          onMouseEnter={() => handleLinkHover(link.image)}
+          onMouseLeave={() => setHoveredOption("")}
         >
           <Link
             className={styles["navbar__link"]}
