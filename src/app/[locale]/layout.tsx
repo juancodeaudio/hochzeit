0b827c09df84ab11d8548d0fa2a1e38aea25e0fe
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-22 17:47:56 -0500

    feat(Localization): :sparkles: Move localized pages for FAQ, Gallery, Home, Party, and Story to locale folder

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,8 @@
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import {routing} from 'app/i18n/routing';
 import { Navbar } from "app/components/shared/Navbar";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
@@ -12,18 +15,26 @@ export const metadata: Metadata = {
   description: "Camila & Juan Pablo's wedding",
 };
 
-export default function RootLayout({
+export default async function LocaleLayout({
   children,
+  params
 }: Readonly<{
   children: React.ReactNode;
+  params: Promise<{locale: string}>;
 }>) {
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>
-        <Navbar />
-        <Header />
-        {children}
-        <Footer />
+        <NextIntlClientProvider>  
+          <Navbar />
+          <Header />
+          {children}
+          <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
