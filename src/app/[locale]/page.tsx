0fed27c37b5108a8f5136be21690704bbbf03e11
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-25 14:15:29 -0500

    feat(Home): :sparkles: Integrate ParallaxBlock, Details, and Timeline components into Home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,15 @@
 import { Hero } from "app/components/home/Hero";
+import { ParallaxBlock } from "app/components/home/ParallaxBlock";
+import { Details } from "app/components/home/Details";
+import { Timeline } from "app/components/home/Timeline";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ParallaxBlock />
+      <Details />
+      <Timeline />
     </main>
   );
 }
