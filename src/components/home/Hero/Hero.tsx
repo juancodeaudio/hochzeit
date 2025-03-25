c691c04f15e97e6ca6cd773bf83c17b7a092e7dd
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-25 14:14:15 -0500

    feat(Home): :sparkles: Refactor Hero component with new layout, animations, and responsive styles

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import Image from 'next/image';
+import { useScroll, useTransform, motion } from 'motion/react';
+import { useRef } from 'react';
 import {useTranslations} from 'next-intl';
 import styles from './Hero.module.scss';
 
 export const Hero = () => {
   const t = useTranslations('Home.Hero');
+  const container = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end start']
+  })
+  
+  const y = useTransform(scrollYProgress, [0, 1], ["0vh", "150vh"])
+  
   return (
     <section className={styles["Hero"]}>
-      <h1>{t('title')}</h1>
+      <div className={styles["Hero__main"]}>
+        <motion.div className={styles["Hero__img-container"]} style={{y}}>
+          <Image src="/images/IMG_2098.jpeg" fill alt="image"/>
+        </motion.div>
+        <h1>{t('title')}</h1>
+        <h3>{t('welcome')}</h3>
+      </div>
+      <div className={styles["Hero__quote-container"]}>
+        <p>{t('quote')}</p>
+        <p>- {t('quoteAuthor')}</p>
+      </div>
     </section>
   );
 }
