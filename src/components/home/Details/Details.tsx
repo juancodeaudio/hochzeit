e6f687c4039d24695627faf4f7b25cbda459ed61
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-26 12:00:57 -0500

    feat(Details): :sparkles: Enhance Details component with grid layout and integrate FlipClockCountdown for date countdown

--- src/components/home/Details/Details.tsx
+++ src/components/home/Details/Details.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import {useFormatter} from 'next-intl';
 import {useTranslations} from 'next-intl';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import styles from './Details.module.scss';
 
 export const Details = () => {
@@ -18,8 +22,20 @@ export const Details = () => {
   format.dateTime(dateTime, {hour: 'numeric', minute: 'numeric'});
   return (
     <section className={styles["Details"]}>
-      <h3>{t('date')}</h3>
-      <p>{formattedDate}</p>
+      <div className={styles["Details__map-container"]}>
+
+      </div>
+      <div className={styles["Details__date-container"]}>
+        <h3>{t('when')}</h3>
+        <p>{formattedDate}</p>
+      </div>
+      <div className={styles["Details__countdown-container"]}>
+        <FlipClockCountdown
+          to={dateTime}
+          className={styles["Details__countdown"]}
+          labels={[t('days').toUpperCase(), t('hours').toUpperCase(), t('minutes').toUpperCase(), t('seconds').toUpperCase()]}
+        />
+      </div>
     </section>
   );
 }
