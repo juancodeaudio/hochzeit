241f7ccfba1bbf8fa90709c1550bd14a2b5c9641
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-26 11:56:43 -0500

    feat(Styles): :art: Improve Sass structure with new abstracts, mixins, and theme variables

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   sassOptions: {
-    additionalData: `@use "app/sass/abstracts/variables";`,
+    additionalData: `@use "app/sass/abstracts" as *;`,
   },
 };
  
