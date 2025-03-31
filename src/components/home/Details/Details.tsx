82a3870546b8937ca9e4cb1ec70b8dc87e09177d
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-30 21:03:56 -0500

    feat(Home): :lipstick: Enhance Details component by adding a ParallaxBlock with a phrase and subtitle, update styles for improved layout and responsiveness, and refactor date handling for countdown functionality.

--- src/components/home/Details/Details.tsx
+++ src/components/home/Details/Details.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import {useFormatter} from 'next-intl';
-import {useTranslations} from 'next-intl';
+import { useEffect, useState, useMemo } from 'react';
+import { useFormatter, useTranslations } from 'next-intl';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 import styles from './Details.module.scss';
@@ -9,7 +9,7 @@ import styles from './Details.module.scss';
 export const Details = () => {
   const t = useTranslations('Home.Details');
   const format = useFormatter();
-  const dateTime = new Date('2025-08-09T17:20:00-05:00');
+  const dateTime = useMemo(() => new Date("2025-08-09T17:20:00-05:00"), []);
  
   // Renders "Nov 20, 2020"
   const formattedDate = format.dateTime(dateTime, {
@@ -17,24 +17,32 @@ export const Details = () => {
     month: 'short',
     day: 'numeric'
   });
+
+  const [clientDateTime, setClientDateTime] = useState<Date | null>(null);
+
+  useEffect(() => {
+    setClientDateTime(dateTime);
+  }, [dateTime]);
  
   // Renders "11:36 AM"
   format.dateTime(dateTime, {hour: 'numeric', minute: 'numeric'});
   return (
     <section className={styles["Details"]}>
       <div className={styles["Details__map-container"]}>
-
+        <div></div>
       </div>
       <div className={styles["Details__date-container"]}>
         <h3>{t('when')}</h3>
         <p>{formattedDate}</p>
       </div>
       <div className={styles["Details__countdown-container"]}>
-        <FlipClockCountdown
-          to={dateTime}
-          className={styles["Details__countdown"]}
-          labels={[t('days').toUpperCase(), t('hours').toUpperCase(), t('minutes').toUpperCase(), t('seconds').toUpperCase()]}
-        />
+        {clientDateTime && (
+          <FlipClockCountdown
+            to={clientDateTime}
+            className={styles["Details__countdown"]}
+            labels={[t('days').toUpperCase(), t('hours').toUpperCase(), t('minutes').toUpperCase(), t('seconds').toUpperCase()]}
+          />
+        )}
       </div>
     </section>
   );
