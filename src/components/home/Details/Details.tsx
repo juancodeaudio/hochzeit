85ab2fea87226c584a86247a4c1014285b231e67
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-03-31 22:06:08 -0500

    feat(Home): :sparkles: Integrate Map component with markers in Details section, add map styles, and enhance layout with new styles for improved user experience.

--- src/components/home/Details/Details.tsx
+++ src/components/home/Details/Details.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import Image from 'next/image';
 import { useEffect, useState, useMemo } from 'react';
 import { useFormatter, useTranslations } from 'next-intl';
+import { Map, Marker } from '@vis.gl/react-maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import mapStyle from 'app/constants/map-styles.json';
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import { motion } from 'motion/react';
 import { Button } from 'app/components/common/Button';
 import styles from './Details.module.scss';
 
@@ -30,7 +35,36 @@ export const Details = () => {
   return (
     <section className={styles["Details"]}>
       <div className={styles["Details__map-container"]}>
-        <div></div>
+        <Map
+          initialViewState={{
+            longitude: -74.069251,
+            latitude: 4.616568,
+            zoom: 17
+          }}
+          style={{width: 600, height: 400, borderRadius: "2rem"}}
+          mapStyle={mapStyle as unknown as string}
+        >
+          {/* <Marker longitude={-74.069251} latitude={4.616568} anchor="bottom" color='#A5AF88' /> */}
+          <Marker longitude={-74.069251} latitude={4.616568} anchor="bottom">
+            <motion.div 
+              animate={{ 
+                y: [0, 0, -40, 0, 0], 
+                scaleY: [1, 0.9, 1.15, 0.8, 1], 
+                scaleX: [1, 1.05, 0.9, 1.1, 1] 
+              }} 
+              transition={{ 
+                repeat: Infinity, 
+                duration: 1.6, 
+                ease: "easeInOut", 
+                times: [0, 0.35, 0.6, 0.85, 1] 
+              }}
+              style={{ originY: 1 }}
+              className={styles["marker"]}
+            >
+              <Image src="/vectors/logo.svg" alt="Ubicación de la boda" width={50} height={50} />
+            </motion.div>
+          </Marker>
+        </Map>
       </div>
       <div className={styles["Details__date-container"]}>
         <h3>{t('when')}</h3>
