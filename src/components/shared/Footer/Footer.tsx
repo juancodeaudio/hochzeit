e372f3503e771c72fe1c5da3d2e6590f3caa7056
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-04-13 18:28:40 -0500

    feat(Footer): :art: Update Footer component styles to enhance visual appeal with new background color, height, and alignment for improved user experience. (Temporal)

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer>
+    <footer style={{height: "50vh", width: "100%", backgroundColor: "#313030", color: "#fefefe", display: "flex", justifyContent: "center", alignItems: "center"}}>
       <p>© 2021 Camila & Juan Pablo</p>
     </footer>
   );
