f2fc3e02d2bb77d3e79b9369e354bc164b2771e1
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-04-13 18:27:00 -0500

    fix(Config): :art: Update image sources in navigation links and timeline years for improved visual consistency and relevance.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,9 +1,9 @@
 export const navLinks = [
-  { href: "/", label: "Home", image: "/images/IMG_1704.jpeg" },
-  { href: "/story", label: "Story", image: "/images/IMG_1338.jpeg", isVertical: true },
-  { href: "/gallery", label: "Gallery", image: "/images/IMG_2098.jpeg" },
-  { href: "/party", label: "Party", image: "/images/IMG_1704.jpeg" },
-  { href: "/QA", label: "QA", image: "/images/IMG_2098.jpeg" },
+  { href: "/", label: "Home", image: "/images/IMG_1890.jpeg" },
+  { href: "/story", label: "Story", image: "/images/IMG_3537.jpeg" },
+  { href: "/gallery", label: "Gallery", image: "/images/IMG_5106.jpeg", isVertical: true },
+  { href: "/party", label: "Party", image: "/images/IMG_7580.jpeg", isVertical: true },
+  { href: "/QA", label: "QA", image: "/images/IMG_1294.jpeg" },
 ];
 
 export const siteMetadata = {
@@ -13,14 +13,14 @@ export const siteMetadata = {
 };
 
 export const timelineYears = [
-  { year: 2016, image: "/images/IMG_2098.jpeg", isVertical: false },
+  { year: 2016, image: "/images/IMG_1890.jpeg", isVertical: false },
   { year: 2017, image: "/images/IMG_1338.jpeg", isVertical: true },
-  { year: 2018, image: "/images/IMG_2098.jpeg", isVertical: false },
+  { year: 2018, image: "/images/IMG_1890.jpeg", isVertical: false },
   { year: 2019, image: "/images/IMG_1338.jpeg", isVertical: true },
-  { year: 2020, image: "/images/IMG_2098.jpeg", isVertical: false },
+  { year: 2020, image: "/images/IMG_1890.jpeg", isVertical: false },
   { year: 2021, image: "/images/IMG_1338.jpeg", isVertical: true },
-  { year: 2022, image: "/images/IMG_2098.jpeg", isVertical: false },
+  { year: 2022, image: "/images/IMG_1890.jpeg", isVertical: false },
   { year: 2023, image: "/images/IMG_1338.jpeg", isVertical: true },
-  { year: 2024, image: "/images/IMG_2098.jpeg", isVertical: false },
+  { year: 2024, image: "/images/IMG_1890.jpeg", isVertical: false },
   { year: 2025, image: "/images/IMG_1338.jpeg", isVertical: true },
 ];
