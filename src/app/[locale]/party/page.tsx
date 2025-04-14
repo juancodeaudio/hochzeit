6514d65307af687f4b5f7b2ebffb6ee361af0118
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-04-14 07:07:47 -0500

    feat(Party): :sparkles: Integrate PageTitle component for improved localization and add translations for the party page title

--- src/app/[locale]/party/page.tsx
+++ src/app/[locale]/party/page.tsx
@@ -1,8 +1,14 @@
+import { useTranslations } from "next-intl";
+
+import { PageTitle } from "app/components/common/PageTitle";
 import { PartyTimeline } from "app/components/party/PartyTimeline";
 
 export default function Party() {
+  const t = useTranslations("Party");
+
   return (
     <main>
+      <PageTitle title={t('title')} size="medium" />
       <PartyTimeline />
     </main>
   );
