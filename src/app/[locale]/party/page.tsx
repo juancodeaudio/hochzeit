00c2efcc2730710b0dd59d8aaf0002deaca99d23
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2025-04-20 19:05:44 -0500

    fix(Party): :bug: Comment components to test error inn production

--- src/app/[locale]/party/page.tsx
+++ src/app/[locale]/party/page.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from "next-intl";
 import { PageTitle } from "app/components/common";
-import { PartyTimeline, Palette, DressCode } from "app/components/party";
-import { paletteColors } from "app/constants/config";
+// import { PartyTimeline, Palette, DressCode } from "app/components/party";
+// import { paletteColors } from "app/constants/config";
 
 export default function Party() {
   const t = useTranslations("Party");
@@ -9,9 +9,9 @@ export default function Party() {
   return (
     <main>
       <PageTitle title={t('title')} size="medium" />
-      <PartyTimeline />
+      {/* <PartyTimeline />
       <Palette colors={paletteColors} />
-      <DressCode />
+      <DressCode /> */}
     </main>
   );
 }
